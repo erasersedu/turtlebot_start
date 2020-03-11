f176907a2ee49412f435cab9b5bb8ed5117a8507
luis_contreras <luis@okadanet.org>
2020-03-11 13:37:16 +0900

    pan tilt joint state publisher test

---
+++ scripts/tf_motors.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32
+from geometry_msgs.msg import TransformStamped
+from dynamixel_msgs.msg import JointState as JointStateDynamixel
+from sensor_msgs.msg import JointState
+import tf
+
+def printHelp():
+    print "HEAD NODE. Options:"
+    print "TODO: Print all argument options"
+
+######################################
+#### MOTOR POSITION  CALLBACK  #######
+def callbackHeadPan(msg):
+    global stateHeadPan
+    stateHeadPan = msg.data.current_pos - msg.data.error
+
+def callbackHeadTilt(msg):
+    global stateHeadTilt
+    stateHeadTilt = msg.data.current_pos - msg.data.error
+
+def main():
+    print "INITIALIZING HEAD NODE..."
+    ###Connection with ROS
+    rospy.init_node("motors_states")
+    jointStates = JointState()
+    jointStates.name = ["head_pan_joint", "head_tilt_joint"]
+    jointStates.position = [0 ,0]
+
+    rospy.Subscriber('/head_pan_controller/state', JointStateDynamixel, self.callbackHeadPan)
+    rospy.Subscriber('/head_tilt_controller/state', JointStateDynamixel, self.callbackHeadTilt)
+
+    pubJointStates = rospy.Publisher("/joint_states", JointState, queue_size = 1)
+
+    global stateHeadPan
+    global stateHeadTilt
+    stateHeadPan = 0
+    stateHeadTilt = 0
+
+    loop = rospy.Rate(30)
+    while not rospy.is_shutdown():
+        jointStates.header.stamp = rospy.Time.now()
+        jointStates.position[0] = stateHeadPan
+        jointStates.position[1] = stateHeadTilt
+        pubJointStates.publish(jointStates)
+        loop.sleep()
+
+if __name__ == '__main__':
+    try:
+        if "--help" in sys.argv:
+            printHelp()
+        elif "-h" in sys.argv:
+            printHelp()
+        else:
+            main()
+    except rospy.ROSInterruptException:
+        pass
